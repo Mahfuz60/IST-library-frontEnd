76ff41f791275bde3d4039288d531dd7cc621223
redoyahmed <muhammadhridoy9618@gmail.com>
2021-12-10 22:46:15 +0600

    footer

--- src/Component/Home/Home.js
+++ src/Component/Home/Home.js
@@ -1,12 +1,14 @@
 import React from "react";
 import HomePage from "../HomePage/HomePage";
 import NavBar from "../NavBar/NavBar";
+import Footer from "../Footer/Footer"
 
 function Home(props) {
   return (
     <div>
       <NavBar></NavBar>
       <HomePage></HomePage>
+      <Footer></Footer>
     </div>
   );
 }
